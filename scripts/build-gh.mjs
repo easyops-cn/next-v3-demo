b54ea39831d6e9017310df3cea5ddb75498e3660
weareoutman <wangshenwei@qq.com>
2023-04-18 11:30:36 +0800

    Generate pages automatically

--- scripts/build-gh.mjs
+++ scripts/build-gh.mjs
@@ -2,41 +2,36 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { createRequire } from "node:module";
 import { existsSync } from "node:fs";
-import { cp, mkdir, rm, writeFile } from "node:fs/promises";
+import { cp, mkdir, readdir, rm, writeFile } from "node:fs/promises";
 
 const require = createRequire(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const distDir = path.join(__dirname, "../dist");
 
-const srcBricksOnlyDistDir = path.join(__dirname, "../packages/bricks-only/dist");
-const srcRenderOnlyDistDir = path.join(__dirname, "../packages/render-only/dist");
-const tgtBricksOnlyDir = path.join(distDir, "bricks-only");
-const tgtRenderOnlyDir = path.join(distDir, "render-only");
+const packagesDir = path.join(__dirname, "../packages");
+const packages = (await readdir(packagesDir, {
+  withFileTypes: true
+})).filter(dir => dir.isDirectory()).map(dir => dir.name);
 
 if (existsSync(distDir)) {
   await rm(distDir, { recursive: true, force: true });
 }
 
 await mkdir(distDir);
-await mkdir(tgtBricksOnlyDir);
-await mkdir(tgtRenderOnlyDir);
 
-await cp(
-  srcBricksOnlyDistDir,
-  tgtBricksOnlyDir,
-  {
-    recursive: true
-  }
-);
-
-await cp(
-  srcRenderOnlyDistDir,
-  tgtRenderOnlyDir,
-  {
-    recursive: true
-  }
-);
+for (const pkg of packages) {
+  const srcDistDir = path.join(packagesDir, `${pkg}/dist`);
+  const tgtDir = path.join(distDir, pkg);
+  await mkdir(tgtDir);
+  await cp(
+    srcDistDir,
+    tgtDir,
+    {
+      recursive: true
+    }
+  );
+}
 
 await writeFile(
   path.join(distDir, ".nojekyll"),
